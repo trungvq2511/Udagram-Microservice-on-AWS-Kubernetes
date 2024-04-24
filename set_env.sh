7b37b333f134da76c5aa3b972590a7cd4cc1989a
cowboysaker2511 <cowboysaker2511@gmail.com>
2024-04-24 13:21:01 +0700

    project 3 - done part 2 refactor to microservices

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=abcd1234
+export POSTGRES_HOST=udagram-trungvq8.cp1sheza8dvp.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-trungvq8-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
---
+++ udagram-api-user/src/server.ts
@@ -0,0 +1,60 @@
+import cors from 'cors';
+import express from 'express';
+import {sequelize} from './sequelize';
+
+import {IndexRouter} from './controllers/v0/index.router';
+
+import bodyParser from 'body-parser';
+import {config} from './config/config';
+import {V0_USER_MODELS} from './controllers/v0/model.index';
+
+
+(async () => {
+  //Check environment variables
+  console.log('POSTGRES_USERNAME=' + process.env.POSTGRES_USERNAME)
+  console.log('POSTGRES_PASSWORD=' + process.env.POSTGRES_PASSWORD)
+  console.log('POSTGRES_HOST=' + process.env.POSTGRES_HOST)
+  console.log('POSTGRES_DB=' + process.env.POSTGRES_DB)
+  console.log('AWS_BUCKET=' + process.env.AWS_BUCKET)
+  console.log('AWS_REGION=' + process.env.AWS_REGION)
+  console.log('AWS_PROFILE=' + process.env.AWS_PROFILE)
+  console.log('JWT_SECRET=' + process.env.JWT_SECRET)
+  console.log('URL=' + process.env.URL)
+  await sequelize.addModels(V0_USER_MODELS);
+
+  console.debug("Initialize database connection...");
+  await sequelize.sync();
+
+  const app = express();
+  const port = process.env.PORT || 8080;
+
+  app.use(bodyParser.json());
+
+  // We set the CORS origin to * so that we don't need to
+  // worry about the complexities of CORS this lesson. It's
+  // something that will be covered in the next course.
+  app.use(cors({
+    allowedHeaders: [
+      'Origin', 'X-Requested-With',
+      'Content-Type', 'Accept',
+      'X-Access-Token', 'Authorization',
+    ],
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    preflightContinue: true,
+    origin: '*',
+  }));
+
+  app.use('/api/v0/', IndexRouter);
+
+  // Root URI call
+  app.get( '/', async ( req, res ) => {
+    res.send( '/api/v0/' );
+  } );
+
+
+  // Start the Server
+  app.listen( port, () => {
+    console.log( `server running ${config.url}` );
+    console.log( `press CTRL+C to stop server` );
+  } );
+})();
